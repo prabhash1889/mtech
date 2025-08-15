36c57b96a67c53aaaa1ec52ac7cb265a1f224d53
prabhash1889 <prabhashpadhan1889@gmail.com>
2025-08-15 22:20:41 +0530

    ssd lab2 start

---
+++ ssd/labs/lab2/q1.sql
@@ -0,0 +1,27 @@
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ListAllSubscribers $$
+CREATE PROCEDURE ListAllSubscribers()
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE v_name VARCHAR(100);
+
+    DECLARE cur CURSOR FOR SELECT SubscriberName FROM Subscribers;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO v_name;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        SELECT v_name AS SubscriberName;
+    END LOOP;
+
+    CLOSE cur;
+END $$
+
+DELIMITER ;
+
+CALL ListAllSubscribers();
